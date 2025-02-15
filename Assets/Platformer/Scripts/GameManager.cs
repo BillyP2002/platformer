817cc4846a17c0abd318c3cf53e24f0569852559
BillyP2002 <bprovins@csumb.edu>
2025-02-15 14:53:47 -0800

    Initial commit

---
+++ Assets/Platformer/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI coinText;
+    public TextMeshProUGUI scoreText;
+    private int coinCount;
+    private int score;
+    public Camera cam;
+    public Transform brickClick;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        coinCount = 0;
+        score = 0;
+        SetCoinCount();
+        SetScoreText();
+    }
+    
+    //Update coin count
+    void SetCoinCount()
+    {
+        coinText.text = "x" + coinCount.ToString();
+    }
+    
+    //Update Score
+    void SetScoreText()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int timeLeft = 300 - (int)(Time.time);
+        timerText.text = $"Time: {timeLeft}"; 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            coinCount = coinCount + 1;
+            SetCoinCount();
+        }
+    }
+    
+    /*if(Input.GetMouseButtonDown(0))
+    {
+        Vector3 screenPos = Input.mousePosition;
+
+        //create a ray that goes through the screenPosition using a camera
+        Ray cursorRay = cam.ScreenPointToRay(screenPos);
+        bool rayHitSomething = Physics.Raycast(cursorRay, out RaycastHit screenHitInfo);
+
+        if (rayHitSomething && screenHitInfo.transform.gameObject.CompareTag("Brick"))
+        {
+            brickClick.position = screenHitInfo.point;
+        }
+    }*/
+    
+}
